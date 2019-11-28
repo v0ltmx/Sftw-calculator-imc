05a2a05322b1973a270528c3ea343c95aaf0a5f1
Lucas Gabriel <37782940+v0ltmx@users.noreply.github.com>
2019-11-27 21:42:21 -0300

    Delete createTable.sql

--- src/persistence/createTable.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS TRABALHO (
-    nome varchar(255) NOT NULL PRIMARY KEY, 
-    altura double,
-    peso double, 
-    idade int,
-    novaconsulta double,
-    antigaconsulta double
-);
